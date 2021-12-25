7988935baa8d56a4873db099bee2e050b961fa3b
Jacob Borella <jacobborella@mbptilhndejacob.lan>
2021-12-25 02:12:46 +0100

    Making links look good in Android

--- src/components/molecules/GameView.js
+++ src/components/molecules/GameView.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 const GameView = (props) => {
   return (
     <View>
-      <Button
-              title={props.data.name + " (" + props.data.variant + ")"}
+      <Text style={{color: 'blue', fontSize:30}}//TODO: dynamic font size
               onPress={() =>
                 props.navigation.navigate('ShowScores', { game: props.data })
-              }
-            />
+              }>{props.data.name + " (" + props.data.variant + ")"}</Text>
       <View
   style={{
     borderBottomColor: 'black',
